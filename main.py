e08b172d58d6fbb69efb5ba2466ee8284ead0000
Arun P Rangrej <arun.rangrej@datagrokr.com>
2021-02-15 17:13:56 +0530

    Commited by Arun P Rangrej

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from flask_mysqldb import MySQL
 app = Flask(__name__)
 
 app.config['MYSQL_HOST'] = '127.0.0.1'
-app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_USER'] = 'arun'
 app.config['MYSQL_PASSWORD'] = 'root'
 app.config['MYSQL_DB'] = 'north_wind'
 
--- test.py
+++ test.py
@@ -5,15 +5,35 @@ BASE = "http://127.0.0.1:5000/"
 responce = requests.get(BASE + "customers/ANTON")
 print(responce.json())
 input()
+
 responce = requests.post(BASE + "customers", {"customers_id": "ALFKI","company_name" : "Alfreds Futterkiste","contact_name":"Maria Anders","contact_title" : "Sales Representative","address": "Obere Str. 57","city":"Berlin","region" : "",
 "postal_code" : "12209","countary" : "Germany","phone" : "030-0074321","fax" : "030-0076545"}
-print(responce)
+print(responce.json())
 input()
+
 responce = requests.put(BASE + "customers/ALFKI", {"company_name" : "Alfreds","contact_name":"Maria","contact_title" : "Sales ","address": " Str. 57","city":"Berlin","region" : "",
 "postal_code" : "12209","countary" : "Germany","phone" : "030-0074321","fax" : "030-0076545"}
-print(responce)
+print(responce.json())
 input()
-#same for product and orders tables
 
+'''-------------------------------------------------------------------------------------'''
+
+responce = requests.get(BASE + "products/3")
+print(responce.json())
+input()
+
+responce = requests.post(BASE + "products", {"ProductID": 111,"ProductName" : "my_Chai","SupplierID":1,"CategoryID" : 1,	"QuantityPerUnit": "10 boxes x 20 bags","UnitPrice":18.00,"UnitsInStock" : 39,
+"UnitsOnOrder" : 0,"ReorderLevel" : 10,"Discontinued" : 0}
+print(responce.json())
+input()
+
+responce = requests.put(BASE + "products/3", {"ProductID": 111,"ProductName" : "TEA","SupplierID":4,"CategoryID" : 1,	"QuantityPerUnit": "20 boxes x 20 bags","UnitPrice":18.00,"UnitsInStock" : 39,
+"UnitsOnOrder" : 0,"ReorderLevel" : 10,"Discontinued" : 0}
+print(responce.json())
+input()
+
+'''-------------------------------------------------------------------------------------'''
+
+#same for order table
 responce = requests.get(BASE + "orderhistory/ANTON")
 print(responce.json())
